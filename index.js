dbed16fe48257143a25fd74cacfcf31476d022f7
Joshua Wallace <josh@boostmyschool.com>
2025-02-06 12:18:16 -0500

    feat: PLT-2168 move to new zipcode api implementation (#1)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const sha1 = require('js-sha1');
+
 function checkStatus(res) {
   if (res.status >= 200 && res.status < 300) {
     return res;
@@ -12,26 +14,54 @@ function ZipCodeApi() {
 }
 
 ZipCodeApi.prototype.init = function(options) {
-  options = options || {};
-  const apiKey = options.apiKey;
+  this.clientKey = null;
+  this.authHash = null;
 
-  if (!apiKey) {
-    throw new Error('Must set an API key');
+  options = options || {};
+  const clientKey = options.clientKey;
+  const apiKey = options.apiKey;
+  const domain = options.domain;
+
+  if (!clientKey) {
+    throw new Error('Must set a client key');
   }
 
-  this.apiKey = apiKey;
+  this.clientKey = clientKey;
+
+  if (!apiKey || !domain) {
+    return;
+  }
+
+  const salt = Array.from({ length: 16 }, () => (Math.random() * 36 | 0).toString(36)).join('');
+  const currentTime = Math.floor(Date.now() / 1000);
+  const hoursUntilExpiration = 1;
+  const sigPrefix = `${currentTime}-${hoursUntilExpiration}-${salt}`;
+  const sigBase = `${sigPrefix}-${domain.toLowerCase()}-${apiKey}`;
+  const hash = sha1(sigBase);
+  const authHash = `${sigPrefix}-${hash}`;
+
+  this.authHash = authHash;
 };
 
-ZipCodeApi.prototype.makeRestUrl = function(suffix) {
-  if (!this.apiKey) {
-    throw new Error('Must set an API key');
+ZipCodeApi.prototype.makeRestUrl = function(zipcode) {
+  if (!this.clientKey) {
+    throw new Error('Must set a client key');
   }
 
-  return 'https://www.zipcodeapi.com/rest/' + this.apiKey + suffix;
+  let endpoint = `https://www.zipcodeapi.com/rest/${this.clientKey}/info.json/${zipcode}/radians`;
+
+  if (this.authHash) {
+    endpoint += `?authHash=${this.authHash}`;
+  }
+
+  // Uncomment for testing
+  // console.log({endpoint});
+
+  return endpoint;
 };
 
 ZipCodeApi.prototype.lookupZipCode = function (zipCode) {
-  return fetch(this.makeRestUrl('/info.json/' + zipCode + '/radians'), {
+  return fetch(this.makeRestUrl(zipCode), {
     headers: {
       Accept: 'application/json',
     },
--- test.js
+++ test.js
@@ -5,8 +5,8 @@ const zipCodeApi = require('./');
 
 describe('zipCodeApi', function () {
   describe('#init', function () {
-    it('complains when an apiKey is not given', function () {
-      expect(zipCodeApi.init).to.throw('Must set an API key');
+    it('complains when a client key is not given', function () {
+      expect(zipCodeApi.init).to.throw('Must set a client key');
     });
   });
 
@@ -15,11 +15,17 @@ describe('zipCodeApi', function () {
 
     beforeEach(function () {
       zipCodeApi.init({
-        // Can use real key and comment out mock to test actual API
+        // Can use real keys and comment out mock to test actual API by logging out the 
+        // genereated url and calling it through codepen using the domain cdpn.io
+        // https://codepen.io/caitlinface/pen/bNbPRwV?editors=1011
         apiKey: 'fake-key',
+        clientKey: 'fake-key',
+        domain: 'fake-domain',
       });
 
-      mock = fetchMock.mock('https://www.zipcodeapi.com/rest/fake-key/info.json/90210/radians', {
+      mock = fetchMock.mock((url, opts) => {
+        return url.startsWith('https://www.zipcodeapi.com/rest/fake-key/info.json/90210')
+      }, {
         city: 'Beverly Hills',
         state: 'CA',
       });
@@ -30,11 +36,39 @@ describe('zipCodeApi', function () {
     });
 
     it('returns city and state', function () {
-      return zipCodeApi.lookupZipCode(90210)
+      return zipCodeApi.lookupZipCode('90210')
         .then(function (res) {
           expect(res.city).to.equal('Beverly Hills');
           expect(res.state).to.equal('CA');
         });
+    });
+  });
+
+  describe('when only clientKey is provided', function () {
+    beforeEach(function () {
+      zipCodeApi.init({
+        clientKey: 'fake-key',
+      });
+    });
+
+    it('generates the url without the authHash', function () {
+      expect(zipCodeApi.makeRestUrl('90210')).to
+        .equal('https://www.zipcodeapi.com/rest/fake-key/info.json/90210/radians');
+    });
+  });
+
+  describe('when clientKey, apiKey, and domain are provided', function () {
+    beforeEach(function () {
+      zipCodeApi.init({
+        apiKey: 'fake-key',
+        clientKey: 'fake-key',
+        domain: 'fake-domain',
+      });
+    });
+
+    it('generates the url with the authHash', function () {
+      expect(zipCodeApi.makeRestUrl('90210')).to
+        .include('https://www.zipcodeapi.com/rest/fake-key/info.json/90210/radians?authHash=');
     });
   });
 });
